6c0874736bca89bf0e015430216d8246357d433e
anonymous-mint <annonymuskmint@gmail.com>
2022-10-25 22:11:05 +0530

    Basic api setup

---
+++ pages/api/blog/index.js
@@ -0,0 +1,5 @@
+const handler  = (req, res) => {
+  res.status(200).json({ message: "success blog" });
+};
+
+export default handler;
---
+++ pages/api/index.js
@@ -0,0 +1,5 @@
+const handler  = (req, res) => {
+  res.status(200).json({ message: "success" });
+};
+
+export default handler;
