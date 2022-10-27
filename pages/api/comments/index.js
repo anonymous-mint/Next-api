2980412848449d14069f96d2164bf2a44869fabf
anonymous-mint <annonymuskmint@gmail.com>
2022-10-27 23:20:07 +0530

    Post api

--- pages/api/comments/index.js
+++ pages/api/comments/index.js
@@ -1,6 +1,16 @@
 import { comments } from "../../../data/comments";
 const Comments = (req, res) => {
-  res.status(200).json(comments);
+  if (req.method === "GET") res.status(200).json(comments);
+  else {
+    const { comment } = req.body;
+    const newComment = {
+      id: Date.now(),
+      text: comment,
+    };
+
+    comments.push(newComment);
+    res.status(200).json(newComment);
+  }
 };
 
 export default Comments;
--- pages/comments/index.js
+++ pages/comments/index.js
@@ -2,16 +2,39 @@ import React, { useState } from "react";
 
 const CommentsList = () => {
   const [commentsList, setCommentsList] = useState([]);
+  const [comment, setComment] = useState("");
+
   const fetchComments = async () => {
     const response = await fetch("/api/comments");
     const data = await response.json();
     console.log(data);
     setCommentsList(data);
   };
+
+  const addComment = async () => {
+    const response = await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({ comment }),
+      headers: { "Content-Type": "application/json" },
+    });
+		const data = await response.json();
+		console.log(data);
+  };
+
   return (
     <div>
       <h1>Click the button to load comments list</h1>
-      <button type="button" onClick={fetchComments}>Load comments</button>
+      <input
+        type="text"
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
+      ></input>
+      <button type="button" onClick={addComment}>
+        Add comments
+      </button>
+      <button type="button" onClick={fetchComments}>
+        Load comments
+      </button>
       {commentsList?.map((comment) => (
         <p key={comment.id}>{comment.text}</p>
       ))}
